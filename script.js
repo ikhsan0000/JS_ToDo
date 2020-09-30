2fc31c9d78bdb0020ad83312f930e4615cafa309
ikhsanfirdauz0 <ikhsan.firdauz@ui.ac.id>
2020-09-30 11:34:15 +0700

    dev build

--- script.js
+++ script.js
@@ -5,49 +5,70 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listener
 todoButton.addEventListener('click', addList);
+todoList.addEventListener('click', deleteCheck);
 
 //Func
 function addList (e)
 {
-    //prevent submit
-    e.preventDefault();    
+    if(todoInput.value !== "")
+    {
+        //prevent submit
+        e.preventDefault();    
 
-    //todo div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
+        //todo div
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
 
-    //create list
-    const newTodo = document.createElement("li");
-    newTodo.innerText = "test";
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+        //create list
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todoInput.value;
+        todoInput.value = "";
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
+        
+        //button group
+        const buttonDiv = document.createElement("div");
+        buttonDiv.classList.add("button-container");
+
+
+        //check button
+        const checkButton = document.createElement("i");
+        checkButton.classList.add('check', 'far', 'fa-check-square', 'fa-2x');
+        buttonDiv.appendChild(checkButton);
+
+
+
+        //delete button
+        const deleteButton = document.createElement("i");
+        deleteButton.classList.add('delete', 'fas', 'fa-trash', 'fa-2x');
+        buttonDiv.appendChild(deleteButton);
+
+
+        todoDiv.appendChild(buttonDiv);
     
-    //button group
-    const buttonDiv = document.createElement("div");
-    buttonDiv.classList.add("button-container");
 
+        // buttonDiv.appendChild(checkButton);
+        // buttonDiv.appendChild(deleteButton);
 
-    //check button
-    const checkButton = document.createElement("i");
-    checkButton.classList.add('far', 'fa-check-square', 'fa-2x', 'check');
-    buttonDiv.appendChild(checkButton);
+        //append to list
+        todoList.appendChild(todoDiv);
+    }
+   
+}
 
-
-
-    //delete button
-    const deleteButton = document.createElement("i");
-    deleteButton.classList.add('fas', 'fa-trash', 'fa-2x', 'delete');
-    buttonDiv.appendChild(deleteButton);
-
-
-    todoDiv.appendChild(buttonDiv);
-  
-
-    // buttonDiv.appendChild(checkButton);
-    // buttonDiv.appendChild(deleteButton);
-
-    //append to list
-    todoList.appendChild(todoDiv);
-
-
+function deleteCheck (e)
+{
+    var target = e.target
+    if(target.classList[0] == 'delete')
+    {
+        target = target.parentElement;
+        target.parentElement.remove();
+    }
+    if(target.classList[0] == 'check')
+    {
+        target.classList.toggle('done');
+        var parent = target.parentElement;
+        var container = parent.parentElement;
+        container.children[0].classList.toggle('completed');
+    }
 }
